f42d416d4cb5f2c21bced0fb0b616d025aab4062
AdrianaAmaral <adrianafsamaral@gmail.com>
2021-11-15 12:43:49 -0300

    rota criado para componente nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: 'app.component.html'
- })
+  templateUrl: 'app.component.html'
+})
 export class AppComponent {
   title = 'projetofinal';
 }
