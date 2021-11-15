53fe23296e377137087364c978badddad8f82010
AdrianaAmaral <adrianafsamaral@gmail.com>
2021-11-15 14:28:55 -0300

    nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+
 import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
