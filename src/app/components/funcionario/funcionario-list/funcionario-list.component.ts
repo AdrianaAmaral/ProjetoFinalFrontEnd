873e114bc81587ba7ade1ca7d8265ed92016bd6c
AdrianaAmaral <adrianafsamaral@gmail.com>
2021-11-15 17:34:28 -0300

    HTML e CSS funcionario-list

--- src/app/components/funcionario/funcionario-list/funcionario-list.component.ts
+++ src/app/components/funcionario/funcionario-list/funcionario-list.component.ts
@@ -1,4 +1,8 @@
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Funcionario } from 'src/app/models/funcionario';
 
 @Component({
   selector: 'app-funcionario-list',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funcionario-list.component.css']
 })
 export class FuncionarioListComponent implements OnInit {
+ 
+  ELEMENT_DATA: Funcionario[] = [
+    {
+      id: 1,
+      nome: 'Adriana Amaral',
+      cpf: '336.255.858.55',
+      email: 'adrianafsamaral@gmail.com',
+      senha: '123',
+      perfis:['0'],
+      dataCriacao: 15/11/2021
+    }
+  ]
+ 
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Funcionario>(this.ELEMENT_DATA);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
